f584a13e6d8ef34819645a801dfd4c48a2737608
Pavel Egorov <pavel.egorov@hodlhodl.com>
2024-05-07 16:36:15 +0300

    added eta

--- main.rb
+++ main.rb
@@ -29,22 +29,42 @@ work = PASSWORD_ARRAY.combination(PASSWORD_SIZE).to_a
 
 total_work = work.size
 
+threads = []
+
 work.each_slice((total_work.to_f / THREAD_NUMBER).ceil).with_index do |slice, index|
-  Process.fork do
+  threads << Process.fork do
     puts "Thread #{index} started"
     puts "Slice size: #{slice.size}"
 
-    slice.each do |key|
+    last_time = Time.now
+
+    slice.each.with_index do |key, i|
+      if index == 0
+        time_now = Time.now
+        eta = (time_now - last_time) * (slice.size - i)
+        puts "#{i}/#{slice.size}, ETA: #{eta.round}s (#{( eta / 60 ).round(2)}m), #{(i.to_f / slice.size * 100).round(2)}%"
+      end
+
       key.permutation(PASSWORD_SIZE).each do |permutated_key|
         decrypted_data = decrypt_data(permutated_key.join)
 
         if decrypted_data
+          puts "-" * 50
           puts "Decrypted data: #{decrypted_data}"
+          puts "-" * 50
           exit(0)
         end
       end
+
+      last_time = time_now if index == 0
     end
   end
 end
 
 Process.wait
+
+threads.each do |thread|
+  Process.kill('KILL', thread) 
+rescue Errno::ESRCH
+  nil
+end
